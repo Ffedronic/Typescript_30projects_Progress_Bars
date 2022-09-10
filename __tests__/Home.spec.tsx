fb84490074b4c145f7cc2804b7dc0df02a98b6f0
felix fedronic <fedronicfelix@gmail.com>
2022-09-10 17:36:47 +0200

    test

--- __tests__/Home.spec.tsx
+++ __tests__/Home.spec.tsx
@@ -5,5 +5,6 @@ import Home from '../pages'
 it('should render home', () => {
   render(<Home />)
   const main = within(screen.getByRole('main'))
+  console.log(screen)
   expect(main.getByRole('heading', { level: 1, name: /progress/i })).toBeDefined()
 })
